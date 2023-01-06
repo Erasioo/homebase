87c57d53112f936d8a3ef86d073d800e8cbd7d60
Robert Powell <121625312+Erasioo@users.noreply.github.com>
2023-01-06 15:50:58 -0500

    Update app.py
    Update #1.1; still working on file handling for the quest log.

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import uuid
 user = None
 
 class User:
-    def __init__(self, user_name, uuid):
+    def __init__(self, user_name, uuid, quest_log):
         self.user_name = user_name
         self.uuid = uuid
-    
+        self.quest_log = quest_log
     
 def new_user():
     global user
-    user = User(input('What is your User Name? '), uuid.uuid4())
+    new_uuid = uuid.uuid4()
+    user = User(input('What is your User Name? '), new_uuid, f'quest_log_{new_uuid}.txt' )
     print(f'\nWelcome {user.user_name}')
 
 
@@ -25,15 +26,29 @@ def start_program():
 
 def main_menu():
     global user
-    print(f'What would you like to do today, {user.user_name}?\n1. [BLANK]\n2. [BLANK]\n3. Settings')
+    print(f'What would you like to do today, {user.user_name}?\n1. Start a Quest\n2. [BLANK]\n3. Settings')
     while True:
         if input('Your Choice: ') == '1':
-            
+            try:
+                stream = open(user.quest_log, 'a+')
+                stream.write(input('Quest title: '))
+                stream.write(input('Exp reward: '))
+                stream.close()
+            except Exception as e:
+                print('Something went wrong', e)
+            try:
+                stream = open(user.quest_log)
+                stream.read(1000)
+                stream.close()
+            except Exception as e:
+                print('Something went wrong', e)
+        break
         if input('Your Choice: ') == '3':
             settings_menu()
             break
         else:
             print('Sorry, I cannot help with that right now')
+
 
 def settings_menu():
     global user
@@ -46,9 +61,11 @@ def settings_menu():
             
         elif user_input == '2':
             print(f'\nYour User ID is {user.uuid}')
-            
+            print(f'\nYour Quest Log file is {user.quest_log}')
+        
         elif user_input == '3':
             main_menu()
+            
         else:
             print('Invalid input')
     
